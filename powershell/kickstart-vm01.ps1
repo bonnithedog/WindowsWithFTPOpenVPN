c516e58229f776e3fd875e76b2dc5035149aecf2
bonnithedog <45508928+bonnithedog@users.noreply.github.com>
2020-11-21 21:57:30 +0100

    ok

--- powershell/kickstart-vm01.ps1
+++ powershell/kickstart-vm01.ps1
@@ -17,7 +17,7 @@ powershell -nop -c "iex(New-Object Net.WebClient).DownloadString('https://gist.g
  #Adds install open VPN and the task for open vpn
  powershell -nop -c "iex(New-Object Net.WebClient).DownloadString('https://gist.githubusercontent.com/bonnithedog/753858a5cfcb1b99ca1427ef37de6b22/raw/92ff6a31437666b361bb67488d7639f0b321b912/openvpn.ps1')"
  
-pnuserwithscrambledpassword.ps1')"
+
 
  #Adds install open VPN and the task for open vpn
  powershell -nop -c "iex(New-Object Net.WebClient).DownloadString('https://gist.githubusercontent.com/bonnithedog/753858a5cfcb1b99ca1427ef37de6b22/raw/cc057e8c8e65ae71ef12e916764871ea68098b45/openvpn.ps1')"
--- windows01.tf
+++ windows01.tf
@@ -95,7 +95,7 @@ protected_settings = <<PROTECTED_SETTINGS
   settings = <<SETTINGS
  {
   "fileUris": [
-                "https://gist.githubusercontent.com/bonnithedog/307640c32cbebbeea3a0fea373ea7984/raw/616a39c5781c638ae02b33b0df4c87d7429b380a/kickstart.ps1"
+                "https://gist.githubusercontent.com/bonnithedog/307640c32cbebbeea3a0fea373ea7984/raw/602946418c275f5ec182eecb3efc1040630c045a/kickstart.ps1"
               ]  
 }
   SETTINGS
