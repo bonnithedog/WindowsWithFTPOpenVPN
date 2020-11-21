a15fc84dd3255042cf4e04711e27b69fd0e1856a
bonnithedog <45508928+bonnithedog@users.noreply.github.com>
2020-11-21 18:42:37 +0100

    Update windows01.tf

--- windows01.tf
+++ windows01.tf
@@ -38,87 +38,87 @@ resource "azurerm_network_interface" "web-windows-vm-nic" {
   }
 }
 
-## Create Windows web Server
-#resource "azurerm_windows_virtual_machine" "web-windows-vm" {
-#  depends_on=[azurerm_network_interface.web-windows-vm-nic]
-#  name = "win-${random_string.random-win-vm01.result}-vm"
-#  location              = azurerm_resource_group.network-rg.location
-#  resource_group_name   = azurerm_resource_group.network-rg.name
-#  size                  = var.web-windows-vm-size
-#  network_interface_ids = [azurerm_network_interface.web-windows-vm-nic.id]
-#  
-#  computer_name = "win-${random_string.random-win-vm01.result}-vm"
-#  admin_username = var.web-windows-admin-username
-#  admin_password = var.web-windows-admin-password
-#  os_disk {
-#    name = "win-${random_string.random-win-vm01.result}-vm-os-disk"
-#    caching              = "ReadWrite"
-#    storage_account_type = "Standard_LRS"
-#  }
-#  source_image_reference {
-#    publisher = "MicrosoftWindowsServer"
-#    offer     = "WindowsServer"
-#    sku       = var.windows-2019-sku
-#    version   = "latest"
-#  }
-#  enable_automatic_updates = true
-#  provision_vm_agent       = true
-#  tags = {
-#    application = var.app_name
-#    environment = var.environment 
-#  }
-#}
+# Create Windows web Server
+resource "azurerm_windows_virtual_machine" "web-windows-vm" {
+  depends_on=[azurerm_network_interface.web-windows-vm-nic]
+  name = "win-${random_string.random-win-vm01.result}-vm"
+  location              = azurerm_resource_group.network-rg.location
+  resource_group_name   = azurerm_resource_group.network-rg.name
+  size                  = var.web-windows-vm-size
+  network_interface_ids = [azurerm_network_interface.web-windows-vm-nic.id]
+  
+  computer_name = "win-${random_string.random-win-vm01.result}-vm"
+  admin_username = var.web-windows-admin-username
+  admin_password = var.web-windows-admin-password
+  os_disk {
+    name = "win-${random_string.random-win-vm01.result}-vm-os-disk"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = var.windows-2019-sku
+    version   = "latest"
+  }
+  enable_automatic_updates = true
+  provision_vm_agent       = true
+  tags = {
+    application = var.app_name
+    environment = var.environment 
+  }
+}
 
 #Install local software
 
-## Virtual Machine Extension to Install Requierd software
-#  resource "azurerm_virtual_machine_extension" "Install-Requierd-Software" {
-#  depends_on=[azurerm_windows_virtual_machine.web-windows-vm]
-#  name = "win-${random_string.random-win-vm01.result}-vm-extension"
-#  virtual_machine_id = azurerm_windows_virtual_machine.web-windows-vm.id
-#  publisher = "Microsoft.Compute"
-#  type = "CustomScriptExtension"
-#  type_handler_version = "1.9"
+# Virtual Machine Extension to Install Requierd software
+  resource "azurerm_virtual_machine_extension" "Install-Requierd-Software" {
+  depends_on=[azurerm_windows_virtual_machine.web-windows-vm]
+  name = "win-${random_string.random-win-vm01.result}-vm-extension"
+  virtual_machine_id = azurerm_windows_virtual_machine.web-windows-vm.id
+  publisher = "Microsoft.Compute"
+  type = "CustomScriptExtension"
+  type_handler_version = "1.9"
+
+
+
+
+protected_settings = <<PROTECTED_SETTINGS
+    {
+       "commandToExecute": "powershell -ExecutionPolicy Unrestricted -File kickstart.ps1"
+        
+
+    }
+  PROTECTED_SETTINGS
+
+
+  settings = <<SETTINGS
+ {
+  "fileUris": [
+                "https://gist.githubusercontent.com/bonnithedog/307640c32cbebbeea3a0fea373ea7984/raw/fe1097505231d3f56cf1deeddd3abc00ea22a7dd/kickstart.ps1"
+              ]  
+}
+  SETTINGS
+
+  
+  tags = {
+    application = var.app_name
+    environment = var.environment
+  }
+
+}
+
+##############################################################################
+# Outputs File
 #
-#
-#
-#
-#protected_settings = <<PROTECTED_SETTINGS
-#    {
-#       "commandToExecute": "powershell -ExecutionPolicy Unrestricted -File kickstart.ps1"
-#        
-#
-#    }
-#  PROTECTED_SETTINGS
-#
-#
-#  settings = <<SETTINGS
-# {
-#  "fileUris": [
-#                "https://gist.githubusercontent.com/bonnithedog/307640c32cbebbeea3a0fea373ea7984/raw/fe1097505231d3f56cf1deeddd3abc00ea22a7dd/kickstart.ps1"
-#              ]  
-#}
-#  SETTINGS
-#
-#  
-#  tags = {
-#    application = var.app_name
-#    environment = var.environment
-#  }
-#
-#}
-#
-###############################################################################
-## Outputs File
-##
-## Expose the outputs you want your users to see after a successful 
-## `terraform apply` or `terraform output` command. You can add your own text 
-## and include any data from the state file. Outputs are sorted alphabetically;
-## use an underscore _ to move things to the bottom. 
-#
-#output "windows_vm_public_ip" {
-#  value = azurerm_public_ip.web-windows-vm-ip
-#}
+# Expose the outputs you want your users to see after a successful 
+# `terraform apply` or `terraform output` command. You can add your own text 
+# and include any data from the state file. Outputs are sorted alphabetically;
+# use an underscore _ to move things to the bottom. 
+
+output "windows_vm_public_ip" {
+  value = azurerm_public_ip.web-windows-vm-ip
+}
 
 
 
